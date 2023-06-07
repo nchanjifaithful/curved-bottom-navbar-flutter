73a51920da299ec35fade87e2e25c30a2e9963dd
nchanjifaithful <faithful.infinity@gmail.com>
2023-06-07 14:50:18 +0100

    Final changes

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:ch6_basics/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Curved Navigation Bar',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
